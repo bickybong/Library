6d90af788513ceef7d0f0df15cde06da9445a0e8
bicky <bickybong@gmail.com>
2022-07-01 12:29:19 +0800

    add form and remove books

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let library = [];
 const booksContainer = document.querySelector('#booksContainer');
 
-
 function book(title, author, page, read){
     this.title = title;
     this.author = author;
@@ -14,9 +13,13 @@ function addBookToLibrary(book){
 }
 
 function bookCard(){
+    booksContainer.textContent='';
+    count = 0
     for (const obj of library){
+        
         const bookCards = document.createElement('div');
-        bookCards.classList.add('bookCards');
+        bookCards.classList.add('bookCards', count);
+        bookCards.classList.add(count);
         booksContainer.appendChild(bookCards);
 
         const title = document.createElement('h2');
@@ -31,19 +34,70 @@ function bookCard(){
 
         const page = document.createElement('div');
         page.classList.add('page');
-        page.textContent = obj.page;
+        page.textContent = obj.page + " pages";
         bookCards.appendChild(page);
 
         const read = document.createElement('div');
         read.classList.add('read');
         read.textContent = obj.read;
         bookCards.appendChild(read);
+
+        const del = document.createElement('button');
+        del.classList.add(count);
+        del.innerHTML = '<span class="material-icons">delete</span>';
+        bookCards.appendChild(del);
+
+        const unread = document.createElement('button');
+        unread.classList.add('unread');
+        unread.innerHTML = '<span class="material-icons">check_box</span>';
+        bookCards.appendChild(unread);
+        del.onclick = ()=>{cardDelete(count)};
+        unread.onclick = () =>{cardRead(count)};
+        ++count;
     }
 }
 
-book1 = new book("Never get lost", "Wilhelm Thaller", 260, "Not read")
+book1 = new book("Never get lost", "Wilhelm Thaller", 260, "Not Read")
 book2 = new book("The Hobbit", "J.R.R. Tolkien", 310, "Read")
 addBookToLibrary(book1)
 addBookToLibrary(book2)
 // console.log(library)
 bookCard()
+
+function openForm() {
+    document.getElementById("popupForm").style.display = "block";
+}
+function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+}
+
+const form = document.querySelector('#formContainer');
+
+form.addEventListener('submit', callbackFunction);
+
+function callbackFunction(event) {
+    event.preventDefault();
+    const myFormData = new FormData(event.target);
+    const formDataObj = Object.fromEntries(myFormData.entries());
+    console.log(formDataObj);
+    addBookToLibrary(formDataObj);
+    bookCard();
+    form.reset();
+    document.getElementById("popupForm").style.display = "none";
+};
+
+
+function cardDelete(count){
+    library.pop(count);
+    bookCard();
+}
+
+function cardRead(count){
+    console.log(library[0]["read"])
+    if (library[count]["read"] === "Read") {
+        library[count]["read"] = "Not Read";
+    } else{
+        library[count]["read"] = "Not Read";
+    }
+    bookCard();
+}
